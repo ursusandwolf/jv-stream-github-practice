5236468231204102914d98ae0a893554addbe12d
Alex <ursusandwolf@protonmail.com>
2026-02-08 23:07:40 +0200

    getOddNumsAverage

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -3,6 +3,9 @@ package practice;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
+
 import model.Candidate;
 import model.Person;
 
@@ -17,6 +20,7 @@ public class StreamPractice {
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
                 .flatMap(s -> Arrays.stream(s.split(",")))
+                .map(String::trim)
                 .mapToInt(Integer::parseInt)
                 .filter(i -> i % 2 == 0)
                 .min()
@@ -31,7 +35,11 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+        return IntStream.range(0, numbers.size())
+                .map(i -> (i % 2 == 0) ? numbers.get(i) - 1 : numbers.get(i))
+                .filter(n -> n % 2 == 0)
+                .average()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     /**
