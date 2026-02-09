a788730ac7cd48b5c91c613d1d850355040fc90d
alex <ursusandwolf@protonmail.com>
2026-02-09 16:35:22 +0200

    CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,13 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+
+    public static final int AGE = 35;
+    public static final String UKRAINIAN = "Ukrainian";
+    public static final int LIVE_FOR = 10;
+
     @Override
     public boolean test(Candidate candidate) {
-        return isValid(candidate, 35, "Ukrainian", 10);
+        return isValid(candidate, AGE, UKRAINIAN, LIVE_FOR);
     }
 
     private static boolean isValid(Candidate c, int age, String nationality, int required) {
-        return c.getAge() > age
+        return c.getAge() >= age
                 && c.isAllowedToVote()
                 && nationality.equals(c.getNationality())
                 && liveMoreThan(c, required);
